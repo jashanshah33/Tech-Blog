de4ed83358ffbd75d07292e34beaf64036d4d61b
jashanshah33 <jashanshah33@gmail.com>
2022-09-12 04:47:15 -0400

    Blog project setup

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAErlEu3ZPHseEIr3VKo7DMqffh9Ym4sMY",
+  authDomain: "react-hooks-blog-9dd09.firebaseapp.com",
+  projectId: "react-hooks-blog-9dd09",
+  storageBucket: "react-hooks-blog-9dd09.appspot.com",
+  messagingSenderId: "131585621539",
+  appId: "1:131585621539:web:bde83dac08d1abb3d5bb96"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+export const firebase = firebase.firestore();
+
+export default firebaseConfig;
